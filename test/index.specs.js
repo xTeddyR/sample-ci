0a6474b3bc4680afbb380003461cecbefc437056
xTeddyR <teddy.reino9@gmail.com>
2016-06-01 18:23:50 +0200

    Add API

--- test/index.specs.js
+++ test/index.specs.js
@@ -4,29 +4,29 @@ var api = require('../index');
 describe('contacts', function () {
     describe('GET /contacts', function () {
         it('should return the list of contacts', function () {
-            return request(api).get('/contacts').send().expect(200);
+            return request(api).get('/api/contacts').send().expect(200);
         });
     });
     
     describe('POST /contacts/:name', function () {
         it('should create a new contact', function () {
-            return request(api).post('/contacts/foo').send().expect(200);
+            return request(api).post('/api/contacts/foo').send().expect(200);
         });
         
         it('should not be authorized to create a new contact with an already register name', function () {
-            return request(api).post('/contacts/exist').send().expect(403);
+            return request(api).post('/api/contacts/exist').send().expect(403);
         });
     });
     
     describe('PUT /contacts/:name/:new', function () {
         it('should update contacts with the same name', function () {
-            return request(api).put('/contacts/foo/bar').send().expect(200);
+            return request(api).put('/api/contacts/foo/bar').send().expect(200);
         });
     });
     
     describe('DELETE /contacts/:name', function () {
         it('should remove contacts with the same name', function () {
-            return request(api).delete('/contacts/foo').send().expect(200);
+            return request(api).delete('/api/contacts/foo').send().expect(200);
         });
     });
 });
